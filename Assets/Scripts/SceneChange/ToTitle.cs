bdb22d7ffea99b9b5949dc920dd8b7f83e9123aa
noguchi-no <hal3152haru@gmail.com>
2022-05-10 10:58:42 +0900

    StageChoice,Build Setting

--- Assets/Scripts/SceneChange/ToTitle.cs
+++ Assets/Scripts/SceneChange/ToTitle.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ToTitle : MonoBehaviour
 {
 
     public void OnClick ()
-    {
-        FadeManager.Instance.LoadScene ("StageChoice", 0.3f);
-        StageManager.useHint = false;
+    {   
+        if(SceneManager.GetActiveScene().name == "StageChoice")
+        {
+            FadeManager.Instance.LoadScene ("Title", 0.3f);
+        }
+        else
+        {
+            FadeManager.Instance.LoadScene ("StageChoice", 0.3f);
+            StageManager.useHint = false;
+        }
+        
     }
 }
