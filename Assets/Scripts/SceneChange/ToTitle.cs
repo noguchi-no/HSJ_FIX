99cb10744865614a70f7a30bd3dc898f02a93d66
tibigame <ryosukehashimoto@tmail.com>
2022-05-15 23:56:42 +0900

    ヒントの修正

--- Assets/Scripts/SceneChange/ToTitle.cs
+++ Assets/Scripts/SceneChange/ToTitle.cs
@@ -7,15 +7,15 @@ public class ToTitle : MonoBehaviour
 {
 
     public void OnClick ()
-    {   
-        if(SceneManager.GetActiveScene().name == "StageChoice")
+    {
+        StageManager.useHint = false;
+        if (SceneManager.GetActiveScene().name == "StageChoice")
         {
             FadeManager.Instance.LoadScene ("Title", 0.3f);
         }
         else
         {
             FadeManager.Instance.LoadScene ("StageChoice", 0.3f);
-            StageManager.useHint = false;
         }
         
     }
--- Assets/Scripts/UI/HintAnime.cs
+++ Assets/Scripts/UI/HintAnime.cs
@@ -36,8 +36,9 @@ public class HintAnime : MonoBehaviour
                     circles[i].transform.DOScale(Vector3.zero, 0.6f).From();
                     yield return new WaitForSeconds(0.1f);
                 }
+                yield return new WaitForSeconds(1.0f);
             }
-            yield return new WaitForSeconds(1.2f);
+            yield return new WaitForSeconds(0.2f);
         }
     }
    
